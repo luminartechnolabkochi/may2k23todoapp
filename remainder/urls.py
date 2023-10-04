3e2c05271805c8f25f747269341bd780d3f55355
sajay <sajaykannan10@gmail.com>
2023-10-04 11:40:52 +0530

    index

--- remainder/urls.py
+++ remainder/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from remainder.views import SignUpView
+from remainder.views import SignUpView,SignInView,IndexView
 
 
 
 urlpatterns=[
 
     path("signup",SignUpView.as_view(),name="signup"),
+    path("signin",SignInView.as_view(),name="signin"),
+    path("index",IndexView.as_view(),name="index")
+
+
     
 
 
--- remainder/views.py
+++ remainder/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from remainder.forms import RegistrationForm,LoginForm
-from django.views.generic import View
+from django.views.generic import View,TemplateView
 from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 # model relation ship
 # 1:1 1:M M:M
 
@@ -30,6 +31,27 @@ class SignInView(View):
         form=LoginForm()
         return render(request,"login.html",{"form":form})
     
+    def post(self,request,*args,**kwargs):
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            uname=form.cleaned_data.get("username")
+            pwd=form.cleaned_data.get("password")
+            usr=authenticate(request,username=uname,password=pwd)   
+            if usr:
+                login(request,usr)
+                messages.success(request,"login success")
+                return redirect("signin")
+            else:
+                messages.error(request,"invalid credentials!!!")
+                return render(request,"login.html",{"form":form})
+            
+class IndexView(TemplateView):
+
+    template_name="index.html"
+
+    
+
+
 
 
 
